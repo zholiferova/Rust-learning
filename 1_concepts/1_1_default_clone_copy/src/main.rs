e16dd22f4d51222ba89393c59dd3a3f5041c8009
Zhanna Oliferova <zoliferova@gmail.com>
2024-05-13 11:58:31 +0300

    Update main.rs

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Implement me!");
+use smart_default::SmartDefault;
+
+#[derive(SmartDefault)]
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub struct Point {
+    #[default = 0.0]
+    x: f64,
+    #[default = 0.0]
+    y: f64,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let p1 = Point::default();
+        let p2 = Point{x: 3.5, y: 2.1,};
+        let p3 = Point{x: 1.0, ..Default::default()};
+        assert!(p1 == Point{x: 0.0, y: 0.0,});
+        assert!(p2 == Point{x: 3.5, y: 2.1,});
+        assert!(p3 == Point{x: 1.0, y: 0.0,});
+    }
 }
